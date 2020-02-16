b4282dd48b25f8582640adb45d89e559a23bf1bd
Hrishikesh Hiraskar <hrishi@uber.com>
2020-02-17 00:29:45 +0530

    [boot-sector] Add Boot Sector Code to print on screen

--- boot-sector.asm
+++ boot-sector.asm
@@ -1,14 +1,22 @@
 ;
-; A simple boot sector program that loops forever.
+; A simple boot sector that prints a message to the screen using a BIOS routine.
 ;
-loop :                      ; Define a label , " loop ", that will allow
-                            ; us to jump back to it , forever.
+mov ah, 0x0e               ; int 10/ ah = 0xeh -> scrolling teletype BIOS routine
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+jmp $                       ; Jump to the current address ( i.e. forever ).
 
-jmp loop                    ; Use a simple CPU instruction that jumps
-                            ; to a new memory address to continue execution.
-                            ; In our case , jump to the address of the current
-                            ; instruction.
-
+;
+; Padding and magic BIOS number.
+;
 times 510 -( $ - $$ ) db 0  ; When compiled , our program must fit into 512 bytes ,
                             ; with the last two bytes being the magic number ,
                             ; so here , tell our assembly compiler to pad out our
