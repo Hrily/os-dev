e90d5c1118dd074b0e6ae7ff85f30cf848afdb20
Hrishikesh Hiraskar <hrishi@uber.com>
2020-02-19 22:40:10 +0530

    [boot-sector] Refactor print library
    This commit:
    	- Refactor print library to provide print function

--- lib/print.asm
+++ lib/print.asm
@@ -2,20 +2,30 @@
 ; A library which provides printing functions
 ;
 
+; function print
+;   prints given string on screen
+;   @param bx address of string
+print:
+	pusha                 ; push all registers to stack
+	mov ah, 0x0e          ; int 10/ah = 0eh -> scrolling teletype BIOS routine
+print__loop_0:
+	mov al, [bx]          ; move char at address bx to al
+	cmp al, 0x0
+	je print__return    ; if (al == 0x0) jump to label print__return
+	int 0x10              ; print interrupt
+	inc bx                ; bx = bx + 1
+	jmp print__loop_0   ; jump to begining of the loop
+print__return:
+	popa
+	ret
+
 ; function print_ln
 ;   prints given string on screen in one line
 ;   @param bx address of string
 print_ln:
-	pusha                 ; push all registers to stack
+	pusha
+	call print
 	mov ah, 0x0e          ; int 10/ah = 0eh -> scrolling teletype BIOS routine
-print_ln_loop_0:
-	mov al, [bx]          ; move char at address bx to al
-	cmp al, 0x0
-	je print_ln_return    ; if (al == 0x0) jump to label print_ln_return
-	int 0x10              ; print interrupt
-	inc bx                ; bx = bx + 1
-	jmp print_ln_loop_0   ; jump to begining of the loop
-print_ln_return:
 	mov al, 0x0a          ; print \n
 	int 0x10
 	mov al, 0x0d          ; print \r
