75ddda9879d396044630889fe11c023c7fea1a57
Hrishikesh Hiraskar <hrishi@uber.com>
2020-02-22 16:13:15 +0530

    [boot-sector] Add function to read disk

--- boot-sector.asm
+++ boot-sector.asm
@@ -1,30 +1,29 @@
 ;
-; A boot sector that prints a string using print function.
+; Boot sector
 ;
-[org 0x7c00]               ; Tell the assembler where this code will be loaded
+[org 0x7c00]                ; Tell the assembler where this code will be loaded
 
-mov bx, HELLO_MSG
-call print_ln
-
-mov bx, GOODBYE_MSG
-call print_ln
-
-mov dx, [HEX_TEST]
-call print_hex
-
-jmp $                      ; Jump to the current address ( i.e. forever ).
+mov [BOOT_DRIVE], dl        ; BIOS stores our boot drive in DL , so it 's
+                            ; best to remember this for later.
+mov bp , 0x8000             ; Here we set our stack safely out of the
+mov sp , bp                 ; way , at 0x8000
+mov bx , 0x9000             ; Load 2 sectors to 0x0000 (ES ):0x9000 (BX)
+mov dh , 2                  ; from the boot disk.
+mov dl , [BOOT_DRIVE]
+call disk_load
+mov dx , [0x9000]           ; Print out the first loaded word , which
+call print_hex              ; we expect to be 0xdada , stored
+                            ; at address 0x9000
+mov dx , [0x9000 + 512]     ; Also , print the first word from the
+call print_hex              ; 2nd loaded sector : should be 0xface
+jmp $
 
 %include "lib/print.asm"
+%include "lib/disk.asm"
 
-; Data
-HELLO_MSG:
-db 'Hello, World!', 0       ; The zero on the end tells our routine
-                            ; when to stop printing characters.
-GOODBYE_MSG:
-db 'Goodbye!', 0
-
-HEX_TEST:
-dw 0xdead
+; Include our new disk_load function
+; Global variables
+BOOT_DRIVE : db 0
 
 ;
 ; Padding and magic BIOS number.
@@ -37,3 +36,10 @@ times 510 -( $ - $$ ) db 0  ; When compiled , our program must fit into 512 byte
 
 dw 0xaa55                   ; Last two bytes ( one word ) form the magic number ,
                             ; so BIOS knows we are a boot sector.
+
+; We know that BIOS will load only the first 512 - byte sector from the disk ,
+; so if we purposely add a few more sectors to our code by repeating some
+; familiar numbers , we can prove to ourselfs that we actually loaded those
+; additional two sectors from the disk we booted from.
+times 256 dw 0xdada
+times 256 dw 0xface
